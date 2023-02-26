245e39bcd1b9efb847c1e42d952ebf6f28ca3bd2
Anderson <andersonlsilva03@gmail.com>
2023-02-26 11:49:03 -0300

    Ajuste final do about me

--- site.js
+++ site.js
@@ -1,6 +1,5 @@
 //Header
-const header = document.querySelector('.header')
-console.log(header)
+const header = document.querySelector('.header');
 
 const navbar = `<div class="navbar__container">
             <a href="index.html" id="navbar__logo"><img src="imagens/logo.png" alt="logo"></a>
@@ -26,9 +25,9 @@ const navbar = `<div class="navbar__container">
                 <a href="https://wa.me/5598984666133" class="button" id="signup"> <i class="fab fa-whatsapp"></i>Entre em contato</a>
               </li>
             </ul>
-          </div>`
+          </div>`;
 
-header.innerHTML += navbar
+header.innerHTML += navbar;
 
 // Display Mobile Menu
 const menu = document.querySelector('#mobile-menu');
@@ -42,14 +41,49 @@ const mobileMenu = () => {
 menu.addEventListener('click', mobileMenu);
 
 //Footer
-const footer = document.querySelector('.footer')
+const footer = document.querySelector('.footer');
 
 const footerContent = `<div class="footer_container">
-                        <h5>Andressa Vandrielly</h5>
+                        
+                          <p class="footer-title">Andressa Vandri</p>
+                          <p>Todos os direitos reservados &copy</p>
+                       
                         <div class="footer_icons_container">
                           <a href="https://wa.me/5598984666133"><i class="fab fa-whatsapp"></i></a>
                           <a href="https://www.instagram.com/andressavandri/"><i class="fab fa-instagram"></i></a>
                         </div>
-                      </div>`
+                      </div>`;
 
-footer.innerHTML += footerContent
+footer.innerHTML += footerContent;
+
+//Services card
+const cards = [
+  {
+    id:1,
+    title: 'Psicoterapia',
+    img: 'imagens/card1.webp',
+    text: 'Atendimentos On-line (+18).',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSdVNWBLrGqoaL_1-42-lzwSKcRmZZlCtCcv2gx36_QGfsCNmg/viewform',
+    linkText: 'Preencha o formulário'
+  }
+];
+
+const serviceCard = document.getElementById('services');
+
+const addCards = () => {
+  const insertServicesCards = cards.map((item) => {
+    return `<div class="card">
+                <h3>${item.title}</h3>
+                <div class="card_container">
+                    <img src=${item.img} alt="${item.title}">
+                    <p>${item.text}</p>
+                    <a href="${item.link}" target="_blank">${item.linkText}</a>
+                </div>
+            </div>`
+  }).join('')
+  console.log(insertServicesCards)
+  serviceCard.innerHTML = insertServicesCards
+}
+
+window.addEventListener('DOMContentLoaded', addCards)
+
